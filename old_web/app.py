2fa77a5bc308aae051b8ccfc4fe89d5762fc7369
Erendz Tarakci <etarakci@berkeley.edu>
2020-08-14 13:32:57 -0700

    combined new web updates with ml full stack dev

---
+++ old_web/app.py
@@ -0,0 +1,60 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+import pickle
+from helper import clean_text
+import pprint 
+import json
+
+# import tensorflow as tf
+# import keras
+# from keras.models import load_model
+pp = pprint.PrettyPrinter(indent=4)
+
+
+app = Flask(__name__)
+
+lyric_model = pickle.load(open('static/models/top30_genre_model.pickle', 'rb'))
+loaded_tfidf= pickle.load(open("static/models/vectorizer.pickle", "rb"))
+genre_dict = pickle.load(open("static/models/genre_dict.csv", "rb"))
+audio_features_model = pickle.load(open("static/models/audio_features_model.csv", "rb"))
+
+# sound_model = load_model('static/models/audio_features_model.h5')
+
+
+@app.route("/", methods=['GET','POST'])
+def index():
+    return render_template("index.html")
+
+@app.route("/genrepredict", methods=['GET','POST'])
+def genrepredict():
+    # figure out how to disect post response
+    if request.method=='POST':
+        text = request.form.get("lyric")
+
+        processed_text = clean_text(text)
+        X_test = loaded_tfidf.transform([processed_text])
+
+        prediction = lyric_model.predict(X_test)
+        prediction_text = genre_dict[prediction[0]]
+        print(prediction_text)
+        return render_template('genre_predict.html', predictiontext=prediction_text, hi="Hi")
+
+    return render_template('genre_predict.html')
+
+
+@app.route("/audiopredict", methods=['GET','POST'])
+def audiopredict():
+    if request.method=='POST':
+        array = request.form.get("inputArray")
+
+        genre_prediction = audio_features_model(array)
+
+    return render_template('quiz_predict.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
